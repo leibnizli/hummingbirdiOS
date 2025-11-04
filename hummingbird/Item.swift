bb06239336eb2a3da3395487a3889ae5a8c354cb
Leibniz Li <stormte@gmail.com>
2025-11-04 21:08:58 +0800

    Initial Commit

---
+++ hummingbird/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  hummingbird
+//
+//  Created by admin on 2025/11/4.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
